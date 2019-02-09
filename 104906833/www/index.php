71e2f0550127849e2135d148f1f5840cc1c964dd
George Zhang <georgezhang@ucla.edu>
2019-02-08 17:44:36 -0800

    index file

--- 104906833/www/index.php
+++ 104906833/www/index.php
@@ -69,49 +69,8 @@ function postPerson($table, $first, $last, $sex, $dob, $dod)
       </div>
     </nav>
 
-    <h1>Add a New Actor/Director</h1>
-    <form action="addPerson.php" method="POST">
-      <div>
-        Type:
-        <input type="radio" name="role" value="Actor" required> Actor
-        <input type="radio" name="role" value="Director"> Director
-      </div>
-      <div>First Name: <input type="text" name="firstname" required></div>
-      <div>Last Name: <input type="text" name="lastname" required></div>
-      <div>
-        Sex:
-        <input type="radio" name="sex" value="Male"> Male
-        <input type="radio" name="sex" value="Female"> Female
-        <input type="radio" name="sex" value="Other"> Other
-      </div>
-      <div>
-        Date of Birth:
-        <input type="date" name="dob" required>
-      </div>
-      <div>
-        Date of Death:
-        <input type="date" name="dod">
-      </div>
-      <input type="submit" value="Submit">
-    </form>
-    <br/>
-    <!-- print out data -->
-    <?php
-    $table = $_POST["role"];
-    $firstname = $_POST["firstname"];
-    $lastname = $_POST["lastname"];
-    $sex = $_POST["sex"];
-    $dob = $_POST["dob"];
-    $dod = $_POST["dod"];
-    if (($table == "Actor" || $table == "Director")
-      && $firstname != "" && $lastname != ""
-      && $dob != "") {
-      if ($table == "Actor"
-        && !($sex == "Male" || $sex == "Female" || $sex == "Other"))
-        print_error("Actor must have a valid gender to be added to site.");
-      else
-        postPerson($table, $firstname, $lastname, $sex, $dob, $dod);
-    }
-    ?>
+    <div style="margin-top: 15px;">
+      <img src="./imgs/imdb.png" alt="" style="width: 100%;">
+    </div>
   </body>
 </html>
