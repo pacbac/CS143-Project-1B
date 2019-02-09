602611e1670902254750960de23fa07e0ee9d301
pacbac <jclaytonc99@gmail.com>
2019-02-08 23:20:39 -0800

    Add new comment page

---
+++ 104906833/www/newComment.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<?php
+include("utils.php");
+?>
+<html>
+  <head>
+    <link rel="stylesheet" type="text/css" media="screen" href="./css/main.css" />
+    <link
+      href="https://fonts.googleapis.com/css?family=Fira+Sans|Source+Sans+Pro:600,700"
+      rel="stylesheet"
+    />
+  </head>
+  <body>
+    <nav>
+      <div><a href="./" id="logo">143MDb</a></div>
+      <div>
+        <div class="dropdown">
+          <button class="dropdown-btn">
+            Browse
+          </button>
+          <div class="dropdown-list">
+            <a href="#">Get Actor Info</a>
+            <a href="#">Get Movie Info</a>
+          </div>
+        </div>
+        <div class="dropdown">
+          <button class="dropdown-btn">
+            Search
+          </button>
+          <div class="dropdown-list">
+            <a href="#">Search</a>
+          </div>
+        </div>
+        <div class="dropdown">
+          <button class="dropdown-btn">
+            Input
+          </button>
+          <div class="dropdown-list">
+            <a href="addPerson.php">Add New Actor/Director</a>
+            <a href="newMovie.php">Add New Movie</a>
+            <a href="newComment.php">Add New Comment</a>
+            <a href="#">Add New Actor to Movie</a>
+            <a href="#">Add New Director to Movie</a>
+          </div>
+        </div>
+      </div>
+    </nav>
+    <h1>Add a New Comment</h1>
+    <form action="newComment.php" method="POST">
+      <div>
+        Username: <input type="text" name="username">
+      </div>
+      <div>
+      </div>
+    </form>
+  </body>
+</html>
